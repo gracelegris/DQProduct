3d62f88ac4e614c14e3ca42b3cba9a5f2dc407d1
UNICEF <UNICEF@MacBook-Air-4.local>
2026-03-26 11:48:47 -0500

    added vaccine schedule change code

--- figs_tables_ppt.R
+++ figs_tables_ppt.R
@@ -81,10 +81,11 @@ intro_paragraph
 ### Schedule
 ## ---------------------------------------------------------------------------------------------------------------------
 
-sched_summary_cty <- sched_summary %>%
+# use schedule summary that only includes the most recent year for each country
+sched_summary_recent_cty <- sched_summary_recent %>%
   filter(iso3c == Current_ISO3)
 
-schedule_table <- sched_summary_cty %>%
+schedule_table <- sched_summary_recent_cty %>%
   # clean up vaccine code for display
   mutate(
     vaccinecode      = str_replace_all(vaccinecode, "_", " "),
@@ -109,6 +110,69 @@ schedule_table <- schedule_table %>%
   column_spec(1, bold = TRUE) %>%
   column_spec(3, width = "1.5cm") %>%
   column_spec(4, width = "5cm")
+
+# check if there have been schedule changes since 2020
+country_sched <- sched_summary %>%
+  filter(iso3c == toupper(Current_ISO3)) %>%
+  arrange(vaccinecode, targetpop, year)
+
+min_yr <- min(country_sched$year, na.rm = TRUE)
+max_yr <- max(country_sched$year, na.rm = TRUE)
+
+# vaccines that appeared in early years but not in the most recent year
+dropped_notes <- country_sched %>%
+  group_by(vaccinecode, targetpop) %>%
+  summarise(last_yr = max(year), first_yr = min(year), .groups = "drop") %>%
+  filter(last_yr < (max_yr - 1)) %>%
+  mutate(
+    clean_vacc = str_replace_all(vaccinecode, "_", " "),
+    note = paste0(clean_vacc, " last recorded on schedule in ", last_yr)
+  ) %>%
+  distinct(note) %>%
+  pull(note)
+
+# changes within vaccines over time
+schedule_changes <- country_sched %>%
+  group_by(vaccinecode, targetpop) %>%
+  mutate(
+    prev_doses    = lag(n_scheduled_doses),
+    prev_ages     = lag(ages_administered),
+    is_new        = is.na(prev_doses) & year > min_yr,
+    doses_changed = !is.na(prev_doses) & (n_scheduled_doses != prev_doses),
+    ages_changed  = !is.na(prev_ages) & (ages_administered != prev_ages),
+    any_change    = is_new | doses_changed | ages_changed
+  ) %>%
+  filter(any_change) %>%
+  mutate(
+    clean_vacc  = str_replace_all(vaccinecode, "_", " "),
+    change_note = case_when(
+      is_new ~ paste0(clean_vacc, " introduced in ", year),
+      doses_changed & ages_changed ~ paste0(
+        clean_vacc, ": doses changed from ", prev_doses, " to ", n_scheduled_doses,
+        ", ages from ", prev_ages, " to ", ages_administered, " (", year, ")"),
+      doses_changed ~ paste0(
+        clean_vacc, ": number of doses changed from ", prev_doses, " to ", n_scheduled_doses, " (", year, ")"),
+      ages_changed ~ paste0(
+        clean_vacc, ": age schedule changed from '", prev_ages, "' to '", ages_administered, "' (", year, ")")
+    )
+  ) %>%
+  ungroup() %>%
+  distinct(change_note) %>%
+  pull(change_note) %>%
+  na.omit()
+
+all_changes <- c(dropped_notes, schedule_changes)
+
+if (length(all_changes) == 0) {
+  schedule_change_note <- paste0("No changes to the ", CountryName, " vaccination schedule were recorded between ", min_yr, " and ", max_yr, ".")
+} else {
+  schedule_change_note <- c(
+    paste0("Schedule changes recorded for ", CountryName, " since ", min_yr, ":"),
+    paste0("  \u2022 ", sort(all_changes))
+  )
+}
+
+cat(schedule_change_note, sep = "\n")
 
 ## ---------------------------------------------------------------------------------------------------------------------
 ### Coverage
